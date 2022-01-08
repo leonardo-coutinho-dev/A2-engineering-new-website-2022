e29de93ff7e29edde4b925faf36e41f5492f3970
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2022-01-08 14:06:00 -0300

    barra de navegação e seção de abertura finalizadas, versão desk, faltando ajustar na versão mobile a seção de abertura

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 /** burguer menu */
 
 const menuBtn = document.querySelector('.menu-btn');
-const navUL = document.querySelector('.nav-ul')
+const navUL = document.querySelector('#nav-ul');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-        navUL.classList.toggle('show');
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-        navUL.classList.toggle('show');
-    }
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    navUL.classList.toggle('show');
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    navUL.classList.toggle('show');
+  }
 });
 
 /** burguer menu */
@@ -26,49 +26,49 @@ let currentSlide = 1;
 // navegação manual do carrossel
 
 let manualNav = function (manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-        btns.forEach((btn) => {
-            btn.classList.remove('active');
-        });
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
     });
+  });
 
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-}
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+};
 
 btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-    });
+  btn.addEventListener('click', () => {
+    manualNav(i);
+    currentSlide = i;
+  });
 });
 
 // javascript for image slider autoplay navigation
 
 let repeat = function (activeClass) {
-    let active = document.getElementsByClassName('active');
-    let i = 1;
+  let active = document.getElementsByClassName('active');
+  let i = 1;
 
-    let repeater = () => {
-        setTimeout(function () {
-            [...active].forEach((activeSlide) => {
-                activeSlide.classList.remove('active');
-            });
+  let repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove('active');
+      });
 
-            slides[i].classList.add('active');
-            btns[i].classList.add('active');
-            i++
-            if (slides.length == i) {
-                i = 0;
-            }
-            if (i >= slides.lenght) {
-                return;
-            }
-            repeater();
-        }, 5000);
-    }
-    repeater();
-}
+      slides[i].classList.add('active');
+      btns[i].classList.add('active');
+      i++;
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.lenght) {
+        return;
+      }
+      repeater();
+    }, 5000);
+  };
+  repeater();
+};
 repeat();
 /** carrossel */
